90b40d827201af21cc95ab23b32e82e37cc52f90
Patrick Ziegler <patzieg1@gmail.com>
2019-09-19 19:57:23 +0200

    Ivh begehe Selbstmord, wenn des ned bald funktioniert.

--- RobotSoftware/MainProgram/MainProgram.ino
+++ RobotSoftware/MainProgram/MainProgram.ino
@@ -9,7 +9,18 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-	// Robot Settings
+	pinMode(A0, OUTPUT);
+}
+
+void loop() {
+	digitalWrite(A0, HIGH);
+	delay(1000);
+	digitalWrite(A0, LOW);
+	delay(1000);
+}
+
+void setup_unused() {
+	/*// Robot Settings
 	JAFTD::RobotSettings robotSettings;
 	robotSettings.mazeMapperSet.ramSSPin = 0;
 
@@ -17,15 +28,19 @@ void setup() {
 	if (robotSetup(robotSettings) == JAFTD::ReturnCode::fatalError)
 	{
 		while (true);
-	}
+	}*/
+	pinMode(A0, OUTPUT);
 }
 
 // The loop function runs over and over again until power down or reset
-void loop() {
-
-	// If robot is completely stuck, just do nothing.
+void loop_unused() {
+	digitalWrite(A0, HIGH);
+	delay(1000);
+	digitalWrite(A0, LOW);
+	delay(1000);
+	/*// If robot is completely stuck, just do nothing.
 	if (JAFTD::robotLoop() == JAFTD::ReturnCode::fatalError)
 	{
 		while (true);
-	}
+	}*/
 }
