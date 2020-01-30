6340e4fc51e7c5cc4e9add72b36b6cb53b94c91b
B.Robots <brobots.graz@gmail.com>
2020-01-30 11:33:26 +0100

    Problem mit Overshoot des Winkels in SensorFusion behoben.

--- RobotSoftware/JAFDProgram/JAFD/source/SensorFusion.cpp
+++ RobotSoftware/JAFDProgram/JAFD/source/SensorFusion.cpp
@@ -24,7 +24,7 @@ namespace JAFD
 		{
 			_fusedData.robotState.wheelSpeeds = MotorControl::getFloatSpeeds();
 			_fusedData.robotState.angularVel = Vec3f((_fusedData.robotState.wheelSpeeds.right - _fusedData.robotState.wheelSpeeds.left) / JAFDSettings::Mechanics::wheelDistance, 0.0f, 0.0f);
-			_fusedData.robotState.rotation += _fusedData.robotState.angularVel / freq;
+			_fusedData.robotState.rotation = Vec3f((MotorControl::getDistance(Motor::right) - MotorControl::getDistance(Motor::left)) / JAFDSettings::Mechanics::wheelDistance, 0.0f, 0.0f);
 			_fusedData.robotState.forwardVel = (_fusedData.robotState.wheelSpeeds.left + _fusedData.robotState.wheelSpeeds.right) / 2.0f;
 			_fusedData.robotState.position += Vec3f(cosf(_fusedData.robotState.rotation.x), sinf(_fusedData.robotState.rotation.x), 0.0f) * (_fusedData.robotState.forwardVel / freq);
 			_fusedData.robotState.mapCoordinate.x = roundf(_fusedData.robotState.position.x / JAFDSettings::Field::cellWidth);
