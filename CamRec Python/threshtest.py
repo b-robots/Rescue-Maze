4f0a080160586a46d59bdc37338fbdabdb4972ab
SaltySebi <sebastian.pfusterer@bulme.at>
2022-04-11 20:28:07 +0200

    Wettbewerbs stand

--- CamRec Python/threshtest.py
+++ CamRec Python/threshtest.py
@@ -7,9 +7,6 @@ import subprocess
 import numpy as np
 import serial
 
-def detect_letter(img):
-    pass
-
 def get_undist_roi(img, map1, map2, is_right=False):
     img = undistort(img, map1, map2)
     if is_right:
@@ -39,35 +36,9 @@ def get_cam_serial():
     rcamId = re.findall("\.usb-1\.3.*\n.*/dev/video[0-9]", response)[0][-1]
     # LeftId is left rightId is right 
     return lcamId, rcamId
-
-def detect_Color(img):
-    """
-    Returrns the detected color in the format: r = 1, y = 2, g = 3, None = 0
-    """
-    pixels = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    sat = pixels[:, :, 1]
-    mask = np.where(np.logical_and(sat > 100, pixels[:, :, 2] > 30), 255, 0)
-    if(np.count_nonzero(mask > 80) / 200.0 / 140.0 > 0.06):
-        hues = np.float32(pixels[np.where(mask > 127)][:, 0])
-        hues *= 2.0
-        hue_mean = np.arctan2(np.mean(np.sin(hues / 180 * np.pi)), np.mean(np.cos(hues / 180 * np.pi)))
-        r_sim = abs(np.arctan2(np.sin(hue_mean - np.pi * 2.0), np.cos(hue_mean - np.pi * 2.0)))
-        y_sim = abs(np.arctan2(np.sin(hue_mean - 0.873), np.cos(hue_mean - 0.873)))
-        g_sim = abs(np.arctan2(np.sin(hue_mean - 1.3), np.cos(hue_mean - 1.3))) 
-        return np.argmin([r_sim, y_sim, g_sim]) + 1
-    
-    return 0
-
-def read_all_serial(port):
-    if port.in_waiting <= 0:
-        return [None]
-    return port.read(port.in_waiting)
-        
+     
 
 def main():
-    port = serial.Serial('/dev/serial0', baudrate=9600, timeout=0)
-    letterLookup = [b'N',  b'H', b'S', b'U']
-    colorLookup = [b'N',  b'R', b'Y', b'G']
 
     map1, map2 = get_undistort_map(0.8, 1.0)
     
@@ -87,9 +58,8 @@ def main():
     camr.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
 
     while True:
+		
         try:
-            if b'B' in read_all_serial(port):
-                port.write(b'OK\n')
             #get picture
             _, imgl = caml.read()
             _, imgr = camr.read()
@@ -97,17 +67,11 @@ def main():
 
             imgr = get_undist_roi(imgr, map1, map2, True)
             imgl = get_undist_roi(imgl, map1, map2, False)
-
-            rightOut = colorLookup[detect_Color(imgr)]
-            leftOut = colorLookup[detect_Color(imgl)]
-        
-        #if rightOut == colorLookup[0]:
-        #    rightOut = letterLookup[detect_letter(imgr)]
-
-        #if leftOut == colorLookup[0]:
-        #    leftOut = letterLookup[detect_letter(imgl)]
-            port.write(b'l' + leftOut + b'r' + rightOut + b'\n')
-            print("l: " + str(leftOut), "r: " + str(rightOut))
+            
+            imgl = cv.cvtColor(imgl, cv.COLOR_BGR2GRAY)
+            imgl = cv.adaptiveThreshold(imgl, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 21, 11)
+            cv.imshow("",imgl)
+            cv.waitKey(1)
         except Exception as e:
             print(e)
             continue
@@ -118,3 +82,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
