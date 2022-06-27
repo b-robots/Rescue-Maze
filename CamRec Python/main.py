72e078b39d3adb4e24e7f7ef148d2c1a349e7dfa
SaltySebi <53235949+SaltySebi@users.noreply.github.com>
2022-06-27 14:37:29 +0200

    finished get features

--- CamRec Python/main.py
+++ CamRec Python/main.py
@@ -3,30 +3,56 @@ from ast import Pass
 from matplotlib.pyplot import gray
 from FisheyeCorrection.undistort import undistort, get_undistort_map
 import cv2 as cv
-import pytesseract
 import re
 import subprocess 
 import numpy as np
-import serial
+#import serial
 import math 
 
 def get_trainData(path, letter):
-    img = cv.imread("../../" + letter + ".jpg")
-    print(img.shape)
+    img = cv.imread(letter + ".jpg")
+    img = cv.resize(img, (320,240))
+    img = 255 - img
     #get roi
     map1, map2 = get_undistort_map(0.8, 1.0)
     img = undistort(img, map1, map2)
-
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow("test1", gray_img)
     #[y:y+h, x:x+w] the ":" means from to 
     #img = img[100:300, 50:190]
-    cv.imwrite("test2.jpg", img)
-    return img
+    
+    return gray_img
 
 def get_features(img):
-    gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(gray_image,(5,5),0)
-    ret3,oth_image = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-    cv.imwrite("test.jpg", oth_image)
+    blur = cv.GaussianBlur(img,(5,5),0)
+    #ret3,oth_image = cv.threshold(blur,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
+    thresh_img = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 21, 4)
+    contours, hierarchy = cv.findContours(thresh_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours = [contours[i] for i in range(len(contours)) if hierarchy[0][i][2] < 0]
+
+    brightest_cnt = None
+    brightest = 0
+    for contour in contours:
+        mask = np.zeros_like(img)
+        cv.fillPoly(mask, [contour], 255)
+        b = np.sum(np.where(mask > 127,img,0)) / (np.sum(mask) + 1)
+        if b > brightest:
+            brightest = b
+            brightest_cnt = contour
+
+    test = np.zeros_like(img)
+    cv.fillPoly(test, [brightest_cnt], 255)
+    cv.imshow("test", test)
+    cv.waitKey(0)
+
+    # Calculate Moments
+    moments = cv.moments(brightest_cnt)
+	# Calculate Hu Moments
+    huMoments = cv.HuMoments(moments)
+    for i in range(0,7):
+        huMoments[i] = -1* math.copysign(1.0, huMoments[i]) * math.log10(abs(huMoments[i]) + 1e-100)
+    
+    return np.squeeze(np.asarray(huMoments))
 
 def detect_letter(features):
     pass
@@ -95,68 +121,68 @@ def get_HU(img):
     
     print(hu_moments)
 
-def main():
-    port = serial.Serial('/dev/serial0', baudrate=9600, timeout=0)
-    letterLookup = [b'N',  b'H', b'S', b'U']
-    colorLookup = [b'N',  b'R', b'Y', b'G']
+# def main():
+#     port = serial.Serial('/dev/serial0', baudrate=9600, timeout=0)
+#     letterLookup = [b'N',  b'H', b'S', b'U']
+#     colorLookup = [b'N',  b'R', b'Y', b'G']
 
-    map1, map2 = get_undistort_map(0.8, 1.0)
+#     map1, map2 = get_undistort_map(0.8, 1.0)
     
-    lcamId, rcamId=get_cam_serial()
-    caml = cv.VideoCapture(int(lcamId)) # left
-    if not caml.isOpened():   
-        raise IOError("Cannot open webcam")
-    caml.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M','J','P','G'))
-    caml.set(cv.CAP_PROP_FRAME_WIDTH, 320)
-    caml.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+#     lcamId, rcamId=get_cam_serial()
+#     caml = cv.VideoCapture(int(lcamId)) # left
+#     if not caml.isOpened():   
+#         raise IOError("Cannot open webcam")
+#     caml.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M','J','P','G'))
+#     caml.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+#     caml.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
 
-    camr = cv.VideoCapture(int(rcamId)) # right
-    if not camr.isOpened():   
-        raise IOError("Cannot open webcam")
-    camr.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M','J','P','G'))
-    camr.set(cv.CAP_PROP_FRAME_WIDTH, 320)
-    camr.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+#     camr = cv.VideoCapture(int(rcamId)) # right
+#     if not camr.isOpened():   
+#         raise IOError("Cannot open webcam")
+#     camr.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M','J','P','G'))
+#     camr.set(cv.CAP_PROP_FRAME_WIDTH, 320)
+#     camr.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
 
-    while True:
-        try:
-            if b'B' in read_all_serial(port):
-                port.write(b'OK\n')
-            #get picture
-            _, imgl = caml.read()
-            _, imgr = camr.read()
-            #undistort and crop
+#     while True:
+#         try:
+#             if b'B' in read_all_serial(port):
+#                 port.write(b'OK\n')
+#             #get picture
+#             _, imgl = caml.read()
+#             _, imgr = camr.read()
+#             #undistort and crop
 
-            imgr = get_undist_roi(imgr, map1, map2, True)
-            imgl = get_undist_roi(imgl, map1, map2, False)
+#             imgr = get_undist_roi(imgr, map1, map2, True)
+#             imgl = get_undist_roi(imgl, map1, map2, False)
 
-            bin_imgl = cv.cvtColor(imgl, cv.COLOR_BGR2GRAY)
-            bin_imgl = cv.adaptiveThreshold(bin_imgl, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 21, 11)
+#             bin_imgl = cv.cvtColor(imgl, cv.COLOR_BGR2GRAY)
+#             bin_imgl = cv.adaptiveThreshold(bin_imgl, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 21, 11)
 
-            bin_imgr = cv.cvtColor(imgr, cv.COLOR_BGR2GRAY)
-            bin_imgr = cv.adaptiveThreshold(bin_imgr, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 21, 11)
+#             bin_imgr = cv.cvtColor(imgr, cv.COLOR_BGR2GRAY)
+#             bin_imgr = cv.adaptiveThreshold(bin_imgr, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV, 21, 11)
 
-            get_HU(bin_imgr)
+#             get_HU(bin_imgr)
 
-            #cv.imwrite("testL.jpg", bin_imgl)
-            #cv.imwrite("testR.jpg", bin_imgr)
+#             #cv.imwrite("testL.jpg", bin_imgl)
+#             #cv.imwrite("testR.jpg", bin_imgr)
 
-            rightOut = colorLookup[detect_Color(imgr)]
-            leftOut = colorLookup[detect_Color(imgl)]
+#             rightOut = colorLookup[detect_Color(imgr)]
+#             leftOut = colorLookup[detect_Color(imgl)]
         
-        #if rightOut == colorLookup[0]:
-        #    rightOut = letterLookup[detect_letter(imgr)]
+#         #if rightOut == colorLookup[0]:
+#         #    rightOut = letterLookup[detect_letter(imgr)]
 
-        #if leftOut == colorLookup[0]:
-        #    leftOut = letterLookup[detect_letter(imgl)]
-            port.write(b'l' + leftOut + b'r' + rightOut + b'\n')
-            print("l: " + str(leftOut), "r: " + str(rightOut))
-        except Exception as e:
-            print(e)
-            continue
+#         #if leftOut == colorLookup[0]:
+#         #    leftOut = letterLookup[detect_letter(imgl)]
+#             port.write(b'l' + leftOut + b'r' + rightOut + b'\n')
+#             print("l: " + str(leftOut), "r: " + str(rightOut))
+#         except Exception as e:
+#             print(e)
+#             continue
 
         
-    #cv.imwrite("testL.jpg", imgl)
-    #cv.imwrite("testR.jpg", imgr)
+#     #cv.imwrite("testL.jpg", imgl)
+#     #cv.imwrite("testR.jpg", imgr)
 
 if __name__ == "__main__":
     img = get_trainData("","U")
